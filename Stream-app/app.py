99078e33a1d90bb415352eebb1f67ef3b5e80c4d
jailrockzz4 <71438284+jailrockzz4@users.noreply.github.com>
2023-02-27 16:51:05 +0530

    Add files via upload

---
+++ Stream-app/app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+
+
+st.title("First Data App !!")
+
+st.subheader("It's my :blue[portfolio!] web app ")
+
+btn1 = st.button("Profile !")
+
+
+if btn1 == True:
+    
+    
+    st.balloons()
+    st.subheader("Hello Amigos !!")
+    st.write("My name is :blue[__JAILSINGH__],Data Science intern at Innomatics Research Labs. I am a B.TECH graduate student from ECE stream,interested in Data Science Technology. I have knowledge in HTML,CSS,JS,bootstap,mysql,matlab and python")
+    st.write("\n")
+    st.write("I did a project on SMART WHEEL CHAIR using respective tools and got certified by the :blue[__IEEE__], My dream is to become an efficient DATA SCIENTIST.")
+    st.write("\n")
+    st.write("\n")
+    st.write("\n")
+
+   
+    st.write("Feel free to connect with me on below links")
+ 
+    st.markdown("[Linkedin](https://www.linkedin.com/in/eslavath-jailsingh-170854194/)")
+    st.markdown("[Github](https://github.com/jailrockzz4)")
+    st.markdown("[mail-to](mailto:eslavathjailsingh@gmail.com)")
